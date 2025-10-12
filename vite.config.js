565e741fcc12a8de49c8390725cf4453bd1ff6c9
Alexander Malikov <malikoff.at@gmail.com>
2025-10-12 20:37:46 +0300

    Обновление config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig((mode) => 
+  {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+  
   plugins: [
     react(),
     tailwindcss(),
@@ -18,8 +22,5 @@ export default defineConfig({
   },
   define: {
     __APP_ENV__: JSON.stringify(env.APP_ENV),
-  },
-
-})
-
-
+  }}
+  })
